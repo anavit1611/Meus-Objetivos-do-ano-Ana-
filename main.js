df7c3323f6609a737dadd6290e4654098180e7c1
anavit1611 <ana.carvalho.silva12@escola.pr.gov.br>
2026-05-09 16:01:21 -0300

    Atualizar main.js

--- main.js
+++ main.js
@@ -14,16 +14,20 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-07-30T00:00:00");
+const tempoObjetivo2 = new Date("2026-09-15T00:00:00");
+const tempoObjetivo3 = new Date("2026-11-20T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-15T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
   let tempoFinal = tempoObjetivo - tempoAtual;
+
+  if (tempoFinal <= 0) {
+    return "Prazo finalizado";
+  }
   
   let segundos = Math.floor(tempoFinal / 1000);
   let minutos = Math.floor(segundos / 60);
@@ -37,6 +41,11 @@ function calculaTempo(tempoObjetivo) {
   return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
 
-for (let i = 0; i < contadores.length; i++) {
+function atualizaContadores() {
+  for (let i = 0; i < contadores.length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i]);
+  }
 }
+
+atualizaContadores();
+setInterval(atualizaContadores, 1000);
